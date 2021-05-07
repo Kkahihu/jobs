75c841fdea13fa60a8c0812387706c2bb83995cc
Kkahihu <kelvinkahihu180226@daystar.ac.ke>
2021-05-07 06:27:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    setJobs(data() {
+
+    },)
+  }, []);
+}
+
+function App() {
   return (
     <div className="App">
        <h1>Hello Sir</h1>
+       {jobs.}
     </div>
   );
 }
