00f05e68e6182062473993e26c6d51e6b4147ab6
Kkahihu <kelvinkahihu180226@daystar.ac.ke>
2021-05-11 11:57:31 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
+import data from './assets/data.json'
 import JobBoardComponent from './components/JobBoardComponent';
 
 
-
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    setJobs(data);
+
+  }, []);
+
+
+  console.log(jobs);
+
+
   return (
     <div className="App">
        <h1>Hello Sir</h1>
