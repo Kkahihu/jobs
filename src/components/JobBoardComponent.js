b004ea51f6b242ea4b08b5a9a40ea3e9a4c0f585
Kkahihu <kelvinkahihu180226@daystar.ac.ke>
2021-09-09 09:12:55 +0300

    Update JobBoardComponent.js
    Added mobile view

--- src/components/JobBoardComponent.js
+++ src/components/JobBoardComponent.js
@@ -29,32 +29,32 @@ const JobBoardComponent = ({
     return (
         <div className={`flex flex-col bg-white shadow-md my-16 mx-10 p-6 rounded ${ 
             featured && 'border-l-8 border-green-500 border-solid'
-            }`}>
+            } sm:flex-row`}>
             <div>
-                <img className='-mt-16 mb-6 w-20 h-20' src= {logo} alt={company} />
+                <img className='-mt-16 mb-6 w-20 h-20 sm:h-24 sm:w-24 sm:my-0' src= {logo} alt={company} />
             </div>
 
             <div className="flex flex-col justify-between ml-4">
                 <h3 className="font-bold text-green-500">
                     {company}
                     {isNew && (
-                        <span className="text-green-400 bg-green-100 font-bold m-2 p-1 rounded-full">New</span>
+                        <span className="text-green-400 bg-green-100 font-bold m-2 p-1 rounded-full uppercase text-sm">New</span>
                         )}
                     {featured && (
-                        <span className="text-white bg-gray-800 font-bold py-1 px-2 rounded-full">Featured</span>
+                        <span className="text-white bg-gray-800 font-bold py-1 px-2 rounded-full uppercase text-sm">Featured</span>
                         )}
                 </h3>
-                <h2 className="font-bold text-xl">
+                <h2 className="font-bold text-xl my-2">
                     {position}
                 </h2>
                 <p className="text-gray-700"> 
                     {postedAt} · {contract} · {location}
                 </p>
             </div>
-            <div className=" flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-solid">
+            <div className=" flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-solid sm:ml-auto sm:border-0 sm:pt-0 sm:mt-0">
                 {tags ? 
                 tags.map((langAndTool) => 
-                <span className="text-green-400 bg-green-100 font-bold m-2 p-2 rounded">{langAndTool}</span>) : ''}
+                <span className="text-green-400 bg-green-100 font-bold mr-4 mb-4 p-2 rounded sm:mb-0">{langAndTool}</span>) : ''}
                 
             </div>
         </div>
