36daa9a325881dacf77cc597234046db09e2b454
Kkahihu <kelvinkahihu180226@daystar.ac.ke>
2021-09-09 09:39:49 +0300

    Filter Function
    Added the filter function for tags which helps in filtering which job tags one may want.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,34 @@ import JobBoardComponent from './components/JobBoardComponent';
 
 function App() {
   const [jobs, setJobs] = useState([]);
+  const [filters, setFilters] = useState([]);
 
   useEffect( () => {
     setJobs(data);
   }, []);
+
+  const filterByTags = ({role, level, tools, languages}) => {
+    if(filters.length === 0) {
+      return true;
+    }
+     const tags = [role, level];
+
+        if (tools) {
+            tags.push(...tools);
+        }
+
+        if (languages) {
+            tags.push(...languages);
+        }
+
+        return tags.some(tag => filters.includes(tag));
+  };
+
+  const handleTagClick = (tag) => {
+    setFilters([...filters, tag]);
+  };
+
+  const filteredJobs = jobs.filter(filterByTags);
 
   return (
     <div className="App">
@@ -19,7 +43,12 @@ function App() {
           jobs.length === 0 ? (
             <p>Jobs are fetching...</p>
           ) : (
-            jobs.map((job) => <JobBoardComponent job={job} key={job.id} />
+            jobs.map((job) => (
+              <JobBoardComponent 
+                job={job} 
+                key={job.id} 
+                handleTagClick={handleTagClick} 
+              />)
             ) 
           )
        }
--- src/components/JobBoardComponent.js
+++ src/components/JobBoardComponent.js
@@ -15,6 +15,7 @@ const JobBoardComponent = ({
             languages,
             tools,
             },
+            handleTagClick,
 }) => {
     const tags = [role, level];
 
@@ -54,7 +55,7 @@ const JobBoardComponent = ({
             <div className=" flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-solid sm:ml-auto sm:border-0 sm:pt-0 sm:mt-0">
                 {tags ? 
                 tags.map((langAndTool) => 
-                <span className="text-green-400 bg-green-100 font-bold mr-4 mb-4 p-2 rounded sm:mb-0">{langAndTool}</span>) : ''}
+                <span onClick={() => handleTagClick(tags)} className="text-green-400 bg-green-100 font-bold mr-4 mb-4 p-2 rounded sm:mb-0">{langAndTool}</span>) : ''}
                 
             </div>
         </div>
